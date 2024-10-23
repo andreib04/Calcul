6d63757bef7eeed8f147c33879cacdf9a42c75dc
Andrei R <andrei_raul04@yahoo.com>
2024-10-23 19:26:01 +0300

    Add project files.

---
+++ Sisteme/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sisteme
+{
+	internal class Program
+	{
+		static int n;
+		static decimal[,] a;
+		static decimal[] b;
+		static decimal[] x;
+		static decimal[] xNew;
+		static decimal epsilon;
+
+		static void Main(string[] args)
+		{
+			MetodaIacobi();
+		}
+
+		static void MetodaIacobi()
+		{
+			n = 3;
+			epsilon = 0.001m;
+			a = new decimal[,]
+			{
+				{ 5, 1, 1 },
+				{ 1, 6, 4 },
+				{ 1, 1, 10 }
+			};
+
+			b = new decimal[] { 10, 4, 7 };
+			x = new decimal[n];
+			xNew = new decimal[n];
+
+			decimal[] u = new decimal[n];
+			decimal[] sum = new decimal[n];
+
+			bool conditie = false;
+
+			for(int i = 0; i < n; i++)
+			{
+				decimal u_ = 0;
+				for(int j = 0; j < n; j++)
+				{
+					//if(j != i)
+					//{
+						
+					//}
+					u[i] += Math.Abs(a[i, j]);
+					if (u[i] <= Math.Abs(a[i,i]))
+					{
+						Console.WriteLine("Nu se poate aplica Iacobi");
+					}
+				}
+			}
+
+			for(int i = 0; i < n; i++)
+			{
+				x[i] = b[i] / a[i, i];
+			}
+
+			int k = 0; 
+			decimal[] x_ = new decimal[n];
+			do
+			{
+				k++;
+
+				for (int i = 0; i < n; i++)
+				{
+					sum[i] = 0;
+					for(int j = 0; j < n; j++)
+					{
+						if(i != j)
+						{
+							sum[i] = sum[i] + (a[i, j] * x[j]);
+						}
+					}
+
+					xNew[i] = (b[i] - sum[i]) / a[i, i];
+
+					if (Math.Abs(xNew[i] - x[i]) > epsilon)
+					{
+						conditie = true;
+					}
+				}
+				
+
+			} while (conditie);
+
+			for(int i = 0; i < n; i++)
+			{
+				Console.WriteLine(i.ToString(), xNew[i]);
+			}
+		}
+	}
+}
---
+++ sisteme_neliniare/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sisteme_neliniare
+{
+	//METODE ITERATIVE 
+	internal class Program
+	{
+		//a,b start / end interval
+		//dda, ddb - derivata dubla a,b
+		static decimal a, b, epsilon, dda, ddb, xn, xn_1;
+		static int n;
+		static void Main(string[] args)
+		{
+			Coarda();
+
+			Console.ReadKey();
+		}
+
+		public static void Coarda()
+		{
+			n = 0;
+			a = 0;
+			b = 1;
+			dda = 0;
+			ddb = 6;
+			epsilon = 1e-26m;
+
+			decimal aux;
+
+			if(f(a) * dda <= 0)
+			{
+				xn_1 = a;
+				aux = b;
+			}
+			else
+			{
+				xn_1 = b;
+				aux = a;
+			}
+
+
+			xn = xn_1 - (f(xn_1) / (f(aux) - f(xn_1))) * (aux - xn_1);
+
+			do
+			{
+				n++;
+				xn_1 = xn;
+				xn = xn_1 - (f(xn_1) / (f(aux) - f(xn_1))) * (aux - xn_1);
+				
+				
+			} while (Math.Abs(xn - xn_1) >= epsilon);
+
+			Console.WriteLine($"{n} {xn}");
+
+
+		}
+
+		static decimal f(decimal x)
+		{
+			return x * x * x - 3 * x + 1;
+		}
+	}
+
+}
